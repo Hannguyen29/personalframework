188b9b3d632e0af4e879fa83f82658a6f66952d6
Hannguyen29 <97657839+Hannguyen29@users.noreply.github.com>
2022-10-26 21:08:55 -1000

    added animation

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
+// OVERLAY NAV MENU SHOW HIDE
 
-    // OPEN AND CLOSE SITE NAV MENU
-    const mysitenav = document.querySelector('.site-nav');
-    const mymenubutton = document.querySelector('.menu-button');
-    mymenubutton.onclick = function () {
-        if (mysitenav.getAttribute('data-navstate') === 'closed') {
-            mysitenav.setAttribute('data-navstate','open');
-        } else {
-            mysitenav.setAttribute('data-navstate','closed');
-        };
-    }; 
+const mymenubutton = document.querySelector('.menu-button');
+const mysitenav = document.querySelector('.site-header .site-nav');
 
-    // SCROLL TRIGGERED ANIMATION
-    const myobserver = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.setAttribute("data-sectionstate", "active");
-        } else {
-            entry.target.setAttribute("data-sectionstate", "inactive");
-        }
-        });
+mymenubutton.onclick = function() {
+    if (mysitenav.getAttribute('data-navstate') === 'open') {
+        mysitenav.setAttribute('data-navstate', 'closed')
+    } else {
+        mysitenav.setAttribute('data-navstate', 'open')
+    }
+};
+
+//  REVEAL ON SCROLL ANIMATION
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.setAttribute("data-sectionstate", "active");
+      } else {
+        entry.target.setAttribute("data-sectionstate", "inactive");
+      }
     });
+  });
   
-    document.querySelectorAll("section").forEach((section) => {
-        myobserver.observe(section);
-    });
-    
-});
+  
+  document.querySelectorAll(".animate-on-scroll").forEach((section) => {
+    myobserver.observe(section);
+  });
